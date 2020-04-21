3691d1b43e122c9ccc08e060af7d8e22b760c171
muzili <muzili@nb-chat.com>
2020-04-21 13:56:12 +0800

    edit proxy

---
+++ src/setupProxy.js
@@ -0,0 +1,15 @@
+const proxy = require('http-proxy-middleware');
+
+module.exports = (app) => {
+  app.use(
+    proxy('/api', {
+      target: 'http://127.0.0.1:8888',
+      changeOrigin: true,
+      ws: true,
+      pathRewrite: {
+        '^/api': ''
+      }
+    })
+  );
+};
+
